652b92cf22d3147a6e8e3df03cf2301e1d298e5f
Xuan Hong <hongtran@vmax.vn>
2021-12-26 13:17:05 +0700

    up file

--- assets/notify.js
+++ assets/notify.js
@@ -195,7 +195,7 @@
         }).then(
             response => response.json()
         ).then((html) => {
-            window.location.href = './thankyou.html?fb_pixel_id=';
+            window.location.href = './thankyou.html?fb_pixel_id=552847195984078,155303900116276';
         });
 
         // return form.submit();
